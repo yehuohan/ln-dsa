c8c601ca38f4b0d2aee4d58049aab907e8c6d9fa
yehuohan <550034086@qq.com>
2017-12-15 12:15:39 +0800

    feat(dsas-code): 添加Bitwise AND...等

---
+++ dsas-code/LeetCode/Counting Bits/Counting Bits.c
@@ -0,0 +1,38 @@
+
+#include <stdio.h>
+#include <malloc.h>
+
+/**
+ * Return an array of size *returnSize.
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* countBits(int num, int* returnSize) {
+	int* rcb = (int*)malloc((num + 1)*sizeof(int));
+	int next = 0;
+	int offset = 0;
+
+    rcb[0] = 0;
+	for (int n = 1; n <= num; n++)
+	{
+		if (n == 1 << offset)
+		{
+			offset++;
+            rcb[n] = 1;
+		}
+		else
+            rcb[n] = rcb[n - (1 << (offset-1))] + 1;
+	}
+
+	*returnSize = num + 1;
+	return rcb;
+}
+
+int main()
+{
+    int size;
+    int* cb = countBits(5, &size);
+    for (int k = 0; k < size; k ++)
+        printf("%d  ", cb[k]);
+    free(cb);
+    return 0;
+}
