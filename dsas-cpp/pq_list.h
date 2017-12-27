12e05d7c2cd235fd4750c41b46d5bd6dbe60120d
yehuohan <550034086@qq.com>
2017-12-27 19:33:41 +0800

    docs(dsas-cpp/pq_list.h): 添加注释

--- dsas-cpp/pq_list.h
+++ dsas-cpp/pq_list.h
@@ -39,6 +39,7 @@ class PqList : public dsa::PQ<T>, public dsa::List<T>
 {
 public:
     PqList() : dsa::PQ<T>(), dsa::List<T>() {}
+    /** 从向量构造优先级队列 */
     PqList(const T* A, int n) : dsa::PQ<T>(), dsa::List<T>() {while(n-- > 0) this->push_front(A[n]);}
 
     /** 使用List的push_back实现插入，时间复杂弃O(1) */
